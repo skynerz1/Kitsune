0290c5f98e7646603300d07432362dfcaa6690dc
skynerz1 <153457906+skynerz1@users.noreply.github.com>
2025-08-06 11:23:53 +0300

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,10 +7,10 @@ const Footer = () => {
     <footer className="w-full bg-base-300 shadow-xl p-5 flex flex-col items-center space-y-5">
       <Image src="/icon.png" alt="logo" width="100" height="100" />
       <div className="flex space-x-5 items-center">
-        <a href="https://github.com/Dovakiin0/Kitsune" target="_blank">
+        <a href="" target="_blank">
           <GitHubLogoIcon width="25" height="25" />
         </a>
-        <a href="https://discord.gg/6yAJ3XDHTt" target="_blank">
+        <a href="" target="_blank">
           <DiscordLogoIcon width="25" height="25" />
         </a>
       </div>
@@ -18,7 +18,7 @@ const Footer = () => {
         Kitsune does not store any files on the server, we only link to the
         media which is hosted on 3rd party services.
       </p>
-      <p className="text-sm text-gray-300">&copy; Kitsune</p>
+      <p className="text-sm text-gray-300">&copy; DFkz</p>
     </footer>
   );
 };
